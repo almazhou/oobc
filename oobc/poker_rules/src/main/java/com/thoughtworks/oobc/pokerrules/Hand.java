e781b7f9120381a709e9d16239cfb2ed88e0b788
vincentx <vincent.hao.xu@gmail.com>
2014-06-16 13:45:00 +0800

    refactoring example for poker rules

--- oobc/poker_rules/src/main/java/com/thoughtworks/oobc/pokerrules/Hand.java
+++ oobc/poker_rules/src/main/java/com/thoughtworks/oobc/pokerrules/Hand.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import static java.util.Collections.max;
 import static java.util.Collections.min;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
     private final List<Card> cards;
 
     public Hand(Card card1, Card card2, Card card3, Card card4, Card card5) {
@@ -17,8 +17,7 @@ public class Hand {
 
         Set<Card.Kind> kinds = cards.stream().map(Card::getKind).collect(Collectors.toSet());
 
-        Set<Integer> numbers = cards.stream().sorted((s1, s2) -> s1.getNumber() - s2.getNumber())
-                .map(Card::getNumber).collect(Collectors.toSet());
+        Set<Integer> numbers = cards.stream().map(Card::getNumber).collect(Collectors.toSet());
 
         Integer max = Collections.max(cards.stream().map(Card::getNumber).collect(Collectors.groupingBy(s -> s))
                 .values().stream().map(s -> s.size()).collect(Collectors.toList()));
@@ -42,6 +41,37 @@ public class Hand {
         if (numbers.size() == 2 && max == 3) return Rank.FullHouse;
 
         return Rank.HighCard;
+    }
+
+    @Override
+    public int compareTo(Hand o) {
+        Rank rank = getRank();
+        int rankCompare = rank.compareTo(o.getRank());
+        if (rankCompare != 0) return rankCompare;
+        if (rank == Rank.Straight || rank == Rank.StraightFlush
+                || rank == Rank.Flush || rank == Rank.HighCard)
+            return getMaxCard().getNumber() - o.getMaxCard().getNumber();
+
+        List<Integer> pairs = getPairs();
+        List<Integer> otherPairs = o.getPairs();
+        for (int i = 0; i < pairs.size(); i++)
+            if (pairs.get(i) - otherPairs.get(i) != 0) return pairs.get(i) - otherPairs.get(i);
+        return 0;
+    }
+
+    private List<Integer> getPairs() {
+        Map<Integer, List<Integer>> collect = cards.stream().map(Card::getNumber).collect(Collectors.groupingBy(s -> s));
+        return collect.keySet().stream().sorted(
+                (a, b) -> {
+                    int result = collect.get(a).size() - collect.get(b).size();
+                    return -(result != 0 ? result : a - b);
+                }
+        ).collect(Collectors.toList());
+    }
+
+
+    private Card getMaxCard() {
+        return cards.stream().max((c1, c2) -> c1.getNumber() - c2.getNumber()).get();
     }
 
     public enum Rank {
--- oobc/poker_rules/src/test/java/com/thoughtworks/oobc/pokerrules/HandTest.java
+++ oobc/poker_rules/src/test/java/com/thoughtworks/oobc/pokerrules/HandTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class HandTest {
     @Test
@@ -119,5 +120,226 @@ public class HandTest {
                 new Card(Card.Kind.Club, 6),
                 new Card(Card.Kind.Heart, 9));
         assertThat(hand.getRank(), is(Hand.Rank.HighCard));
+    }
+
+    @Test
+    public void higher_rank_should_be_greater_than_low_rank() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Spade, 1),
+                new Card(Card.Kind.Heart, 1),
+                new Card(Card.Kind.Diamond, 1),
+                new Card(Card.Kind.Club, 5),
+                new Card(Card.Kind.Heart, 5));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 1),
+                new Card(Card.Kind.Heart, 3),
+                new Card(Card.Kind.Diamond, 5),
+                new Card(Card.Kind.Club, 6),
+                new Card(Card.Kind.Heart, 9));
+
+        assertTrue(hand1.compareTo(hand2) > 0);
+    }
+
+    @Test
+    public void royal_flush_should_be_equal() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 1),
+                new Card(Card.Kind.Heart, 13),
+                new Card(Card.Kind.Heart, 12),
+                new Card(Card.Kind.Heart, 11),
+                new Card(Card.Kind.Heart, 10));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 1),
+                new Card(Card.Kind.Spade, 13),
+                new Card(Card.Kind.Spade, 12),
+                new Card(Card.Kind.Spade, 11),
+                new Card(Card.Kind.Spade, 10));
+
+        assertTrue(hand1.compareTo(hand2) == 0);
+    }
+
+    @Test
+    public void straight_flush_compare_greatest_card() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Heart, 8),
+                new Card(Card.Kind.Heart, 7),
+                new Card(Card.Kind.Heart, 6),
+                new Card(Card.Kind.Heart, 5));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Spade, 8),
+                new Card(Card.Kind.Spade, 7),
+                new Card(Card.Kind.Spade, 6));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void straight_compare_greatest_card() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 8),
+                new Card(Card.Kind.Heart, 7),
+                new Card(Card.Kind.Spade, 6),
+                new Card(Card.Kind.Heart, 5));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 8),
+                new Card(Card.Kind.Heart, 7),
+                new Card(Card.Kind.Spade, 6));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void flush_compare_greatest_card() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Heart, 8),
+                new Card(Card.Kind.Heart, 4),
+                new Card(Card.Kind.Heart, 3),
+                new Card(Card.Kind.Heart, 2));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Spade, 8),
+                new Card(Card.Kind.Spade, 4),
+                new Card(Card.Kind.Spade, 2));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void high_card_compare_greatest_card() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 8),
+                new Card(Card.Kind.Heart, 4),
+                new Card(Card.Kind.Spade, 3),
+                new Card(Card.Kind.Heart, 2));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 8),
+                new Card(Card.Kind.Heart, 4),
+                new Card(Card.Kind.Spade, 2));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void three_of_a_kind_compare_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 9),
+                new Card(Card.Kind.Spade, 3),
+                new Card(Card.Kind.Heart, 2));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 10),
+                new Card(Card.Kind.Club, 10),
+                new Card(Card.Kind.Heart, 4),
+                new Card(Card.Kind.Spade, 2));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void full_house_compare_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 9),
+                new Card(Card.Kind.Spade, 3),
+                new Card(Card.Kind.Heart, 3));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 10),
+                new Card(Card.Kind.Club, 10),
+                new Card(Card.Kind.Heart, 4),
+                new Card(Card.Kind.Spade, 4));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void four_of_a_kind_compare_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 9),
+                new Card(Card.Kind.Club, 9),
+                new Card(Card.Kind.Heart, 3));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 10),
+                new Card(Card.Kind.Club, 10),
+                new Card(Card.Kind.Diamond, 10),
+                new Card(Card.Kind.Spade, 4));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void two_pair_compare_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 10),
+                new Card(Card.Kind.Club, 10),
+                new Card(Card.Kind.Heart, 3));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 10),
+                new Card(Card.Kind.Club, 11),
+                new Card(Card.Kind.Diamond, 11),
+                new Card(Card.Kind.Spade, 4));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void two_pair_compare_card_if_same_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 10),
+                new Card(Card.Kind.Club, 10),
+                new Card(Card.Kind.Heart, 3));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Club, 9),
+                new Card(Card.Kind.Diamond, 9),
+                new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 10),
+                new Card(Card.Kind.Spade, 4));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void one_pair_compare_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 10),
+                new Card(Card.Kind.Club, 7),
+                new Card(Card.Kind.Heart, 3));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Spade, 10),
+                new Card(Card.Kind.Heart, 10),
+                new Card(Card.Kind.Club, 12),
+                new Card(Card.Kind.Diamond, 11),
+                new Card(Card.Kind.Spade, 4));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
+    }
+
+    @Test
+    public void one_pair_compare_card_if_same_pair() {
+        Hand hand1 = new Hand(new Card(Card.Kind.Heart, 9),
+                new Card(Card.Kind.Spade, 9),
+                new Card(Card.Kind.Diamond, 10),
+                new Card(Card.Kind.Club, 7),
+                new Card(Card.Kind.Heart, 3));
+
+        Hand hand2 = new Hand(new Card(Card.Kind.Club, 9),
+                new Card(Card.Kind.Diamond, 9),
+                new Card(Card.Kind.Spade, 11),
+                new Card(Card.Kind.Heart, 8),
+                new Card(Card.Kind.Spade, 4));
+
+        assertTrue(hand2.compareTo(hand1) > 0);
     }
 }
