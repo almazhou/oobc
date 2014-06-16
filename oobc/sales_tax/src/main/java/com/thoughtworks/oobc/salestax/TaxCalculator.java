6e6c4dbcddd4a96a48c19193e2861d05a558286b
almazhou <zx_new@126.com>
2014-06-16 18:05:50 +0800

    [zhouxuan] Extract magic number out from TaxCalculator

--- oobc/sales_tax/src/main/java/com/thoughtworks/oobc/salestax/TaxCalculator.java
+++ oobc/sales_tax/src/main/java/com/thoughtworks/oobc/salestax/TaxCalculator.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TaxCalculator {
+    public static final double BASIC_TAX = 0.1;
+    public static final double DUTY = 0.05;
     private final List<Product.Category> exempt;
 
     public TaxCalculator(Product.Category... exempt) {
@@ -11,9 +13,9 @@ public class TaxCalculator {
     }
 
     public double calculate(Product product) {
-        double tax = 0.1;
+        double tax = BASIC_TAX;
         if (exempt.contains(product.getCategory())) tax = 0.0;
-        if (product.getSource() == Product.Source.Imported) tax += 0.05;
+        if (product.getSource() == Product.Source.Imported) tax += DUTY;
         return product.getPrice() * tax;
     }
 }
